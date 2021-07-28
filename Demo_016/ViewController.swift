fb025b5fa7054556f4cbe58463f60724df939545
cowton0627 <cowton0517@gmail.com>
2021-07-28 12:15:19 +0800

    2021 0728 1215

--- Demo_016/ViewController.swift
+++ Demo_016/ViewController.swift
@@ -7,13 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var myScrollView: UIScrollView!
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var textFieldUseProgrammingDelegate: UITextField!
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        myScrollView.setContentOffset(CGPoint(x: 0, y: 336), animated: true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        textField.resignFirstResponder()
+        textField.endEditing(true)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        myScrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        myTextField.delegate = self
+        textFieldUseProgrammingDelegate.delegate = self
+        
+//        let center:NotificationCenter = NotificationCenter.default
+//        center.addObserver(self, selector: #selector(keyboardShown),
+//                          name: UIResponder.keyboardWillShowNotification,
+//                          object: nil)
+        
     }
-
-
+    
+    @objc func keyboardShown(notification: Notification) {
+        let info: NSDictionary = notification.userInfo! as NSDictionary
+        //取得鍵盤尺寸
+        let keyboardSize = (info[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        print(keyboardSize.height)
+    }
+    
+//    //MARK:釋放鍵盤監聽通知
+//     func releaseNotification(){
+//      NotificationCenter.default.removeObserver(self)
+//     }
+    
 }
 
