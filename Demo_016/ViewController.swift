8c5142c9fdf5c8b5ea67f2b704495855a04083a5
Charles Cheng <charles_cheng@microjet.com.tw>
2023-08-14 13:25:35 +0800

    加入不同鍵盤高度的判斷

--- Demo_016/ViewController.swift
+++ Demo_016/ViewController.swift
@@ -7,13 +7,74 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController {
     @IBOutlet weak var myScrollView: UIScrollView!
-    @IBOutlet weak var myTextField: UITextField!
-    @IBOutlet weak var textFieldUseProgrammingDelegate: UITextField!
+    @IBOutlet weak var secondView: UIView!
+    @IBOutlet weak var cTextField: UITextField!
+    @IBOutlet weak var bTextField: UITextField!
+    @IBOutlet weak var aTextField: UITextField!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        aTextField.delegate = self
+        cTextField.delegate = self
+        hideKeyboardWhenTappedAround()
+
+        
+        let center: NotificationCenter = NotificationCenter.default
+        center.addObserver(self,
+                           selector: #selector(keyboardShown),
+                           name: UIResponder.keyboardWillShowNotification,
+                           object: nil)
+    }
+    
+    
+    private func hideKeyboardWhenTappedAround() {
+        let tapAround: UITapGestureRecognizer =
+        UITapGestureRecognizer(target: self,
+                               action: #selector(dismissKeyboard))
+        tapAround.cancelsTouchesInView = false
+        view.addGestureRecognizer(tapAround)
+    }
+
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    @objc func keyboardShown(notification: Notification) {
+        let info: NSDictionary = notification.userInfo! as NSDictionary
+        // 取得鍵盤尺寸
+        let keyboardSize = (info[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        print(keyboardSize.height)
+    }
+    
+    
+    
+//    // 釋放鍵盤監聽通知
+//     func releaseNotification() {
+//      NotificationCenter.default.removeObserver(self)
+//     }
+}
+
+extension ViewController: UITextFieldDelegate {
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
-        myScrollView.setContentOffset(CGPoint(x: 0, y: 336), animated: true)
+        var height: CGFloat = 0
+        let screenHeight = UIScreen.main.bounds.height
+        let offsetY: CGFloat = 20
+        switch textField {
+        case aTextField:
+            height = aTextField.frame.maxY - (screenHeight - 336) + offsetY
+        case bTextField:
+            height = bTextField.frame.maxY - (screenHeight - 336) + offsetY
+        case cTextField:
+            height = cTextField.frame.maxY - (screenHeight - 336) + offsetY
+        default: break
+        }
+        myScrollView.setContentOffset(CGPoint(x: 0, y: height), animated: true)
+
+//        myScrollView.setContentOffset(CGPoint(x: 0, y: 336), animated: true)
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -24,30 +85,5 @@ class ViewController: UIViewController, UITextFieldDelegate {
     func textFieldDidEndEditing(_ textField: UITextField) {
         myScrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        myTextField.delegate = self
-        textFieldUseProgrammingDelegate.delegate = self
-        
-//        let center:NotificationCenter = NotificationCenter.default
-//        center.addObserver(self, selector: #selector(keyboardShown),
-//                          name: UIResponder.keyboardWillShowNotification,
-//                          object: nil)
-        
-    }
-    
-    @objc func keyboardShown(notification: Notification) {
-        let info: NSDictionary = notification.userInfo! as NSDictionary
-        //取得鍵盤尺寸
-        let keyboardSize = (info[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
-        print(keyboardSize.height)
-    }
-    
-//    //MARK:釋放鍵盤監聽通知
-//     func releaseNotification(){
-//      NotificationCenter.default.removeObserver(self)
-//     }
-    
 }
 
